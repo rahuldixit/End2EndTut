36307c5806ec93341c683aa8314888b672363154
Rahul Dixit <rahul.dixit.zero@gmail.com>
2016-02-17 20:16:41 +1300

    my tut

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+using MvcApplication1.Models;
+namespace MvcApplication1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcApplication1.TodoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MvcApplication1.TodoContext context)
+        {
+
+            var r = new Random();
+            var items = Enumerable.Range(1, 50).Select(o => new TodoItem
+            {
+                DueDate = new DateTime(2012, r.Next(1, 12), r.Next(1, 28)),
+                Priority = (byte)r.Next(10),
+                Todo = o.ToString()
+            }).ToArray();
+            context.TodoItems.AddOrUpdate(item => new { item.Todo }, items);
+        }
+    }
+}
---
+++ Scripts/Todo/app.js
@@ -0,0 +1,14 @@
+﻿var TodoApp = angular.module("TodoApp", ["ngResource"]).
+    config(function ($routeProvider) {
+        $routeProvider.
+            when('/', { controller: ListCtrl, templateUrl: 'List.html' }).
+            otherwise({ redirectTo: '/' });
+    });
+
+TodoApp.factory('Todo', function ($resource) {
+    return $resource('/api/todo/:id', { id: '@id' }, { update: { method: 'PUT' } });
+});
+
+var ListCtrl = function ($scope, $location, Todo) {
+    $scope.items = Todo.query();
+};
